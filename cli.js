25e888a42bebd2823f7dcc8aadf2d9e1ce1cc1d4
CodeDotJS <rushforlinux@gmail.com>
2016-11-06 00:31:36 +0530

    builds

--- cli.js
+++ cli.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 
-const ipi = require('./index.js');
+const ipp = require('./index.js');
 
 ipi('9gag').then(user => {
 	console.log(user);
 });
 
-ipi.medium('9gag').then(user => {
+ipp.medium('9gag').then(user => {
 	console.log(user);
 });
 
-ipi.small('9gag').then(user => {
+ipp.small('9gag').then(user => {
 	console.log(user);
 });
