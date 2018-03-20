1b43d8582f4b51873c36219b4e04e1b6746e3def
Rishi Giri <rushforlinux@gmail.com>
2018-03-20 12:59:33 +0530

    update tests

--- test.js
+++ test.js
@@ -4,29 +4,29 @@ import fn from './';
 test('full', async t => {
 	const ipp = await fn('iama_rishi');
 
-	t.is(ipp, 'https://scontent.cdninstagram.com/t51.2885-19/17125602_1383250351733245_2023684664551538688_a.jpg');
+	t.is(ipp, 'https://scontent-lga3-1.cdninstagram.com/19440301fb5b335423628339609b6896/5B395F35/t51.2885-19/s1080x1080/28766707_802539189946973_8583808796109832192_n.jpg');
 });
 
 test('medium', async t => {
 	const med = await fn.medium('iama_rishi');
 
-	t.is(med, 'https://scontent.cdninstagram.com/t51.2885-19/s320x320/17125602_1383250351733245_2023684664551538688_a.jpg');
+	t.is(med, 'https://instagram.fpat1-1.fna.fbcdn.net/vp/19440301fb5b335423628339609b6896/5B395F35/t51.2885-19/s320x320/28766707_802539189946973_8583808796109832192_n.jpg');
 });
 
 test('small', async t => {
 	const sm = await fn.small('iama_rishi');
 
-	t.is(sm, 'https://scontent.cdninstagram.com/t51.2885-19/s150x150/17125602_1383250351733245_2023684664551538688_a.jpg');
+	t.is(sm, 'https://instagram.fpat1-1.fna.fbcdn.net/vp/3d1cf3d9f9504bf031c1056878b20412/5B4EA3C5/t51.2885-19/s150x150/28766707_802539189946973_8583808796109832192_n.jpg');
 });
 
 test('image', async t => {
 	const img = await fn.image('https://www.instagram.com/p/BRfTRCQBeHw/');
 
-	t.is(img, 'https://scontent.cdninstagram.com/t51.2885-15/e35/17265889_1909059605990282_9017349928908750848_n.jpg');
+	t.is(img, 'https://instagram.fpat1-1.fna.fbcdn.net/vp/7656198f907d4463bd258511d8905d3d/5B38B5B2/t51.2885-15/e35/17265889_1909059605990282_9017349928908750848_n.jpg');
 });
 
 test('video', async t => {
 	const vid = await fn.video('https://www.instagram.com/p/BTTuhDsgtCs/');
 
-	t.is(vid, 'https://scontent.cdninstagram.com/t50.2886-16/18190450_471341926537056_7672813376128417792_n.mp4');
+	t.is(vid, 'https://instagram.fpat1-1.fna.fbcdn.net/vp/ce165cb680827bea41f2b4d5783e6c41/5AB39926/t50.2886-16/18190450_471341926537056_7672813376128417792_n.mp4');
 });
