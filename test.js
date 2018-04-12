1120d1fef5ddf1fdeba8713c432f335864411d8f
Rishi Giri <rushforlinux@gmail.com>
2018-04-12 16:40:27 +0530

    add test for updated features

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ import fn from './';
 test('full', async t => {
 	const ipp = await fn('iama_rishi');
 
-	t.is(ipp, 'https://scontent-iad3-1.cdninstagram.com/vp/4005e44adebfe7a8fb985593e41c86ab/5B5BAE8C/t51.2885-19/29090932_588149134878706_555805880914804736_n.jpg');
+	t.is(ipp, 'https://scontent-atl3-1.cdninstagram.com/vp/cb21b91f00b3971ca9c7b5f4f11c26e3/5B50A9F0/t51.2885-19/30589937_859363860938724_791137326725070848_n.jpg');
 });
 
 test('medium', async t => {
@@ -16,13 +16,13 @@ test('medium', async t => {
 test('small', async t => {
 	const sm = await fn.small('iama_rishi');
 
-	t.is(sm, 'https://scontent-iad3-1.cdninstagram.com/vp/718ca75b8b173c6fa89af70ccb93afb9/5B533938/t51.2885-19/s150x150/29090932_588149134878706_555805880914804736_n.jpg');
+	t.is(sm, 'https://scontent-atl3-1.cdninstagram.com/vp/67eacbe0f312f272ec8d9c232e4a0ae6/5B734644/t51.2885-19/s150x150/30589937_859363860938724_791137326725070848_n.jpg');
 });
 
 test('image', async t => {
 	const img = await fn.image('https://www.instagram.com/p/BRfTRCQBeHw/');
 
-	t.is(img, 'https://scontent-iad3-1.cdninstagram.com/vp/7656198f907d4463bd258511d8905d3d/5B38B5B2/t51.2885-15/e35/17265889_1909059605990282_9017349928908750848_n.jpg');
+	t.is(img, 'https://scontent-atl3-1.cdninstagram.com/vp/1766f29af7b73e8f1bc2acbc2732525a/5B6042B2/t51.2885-15/e35/17265889_1909059605990282_9017349928908750848_n.jpg');
 });
 
 test('video', async t => {
