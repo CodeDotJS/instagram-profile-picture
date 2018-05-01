7e127292709fd2b7e79c2bd921f323d39e0b8b00
Rishi Giri <rushforlinux@gmail.com>
2018-05-02 05:20:32 +0530

    remove json

--- user.js
+++ user.js
@@ -3,7 +3,12 @@
 const got = require('got');
 
 module.exports = username => {
-	return got(`https://instagram.com/${username}/?__a=1`, {json: true}).then(res => {
-		return res.body.graphql.user.id;
+	return got(`https://instagram.com/${username}`).then(res => {
+		const data = res.body.split('"id":"')[1].split('",')[0];
+		return data;
+	}).catch(err => {
+		if (err) {
+			return err.message;
+		}
 	});
 };
